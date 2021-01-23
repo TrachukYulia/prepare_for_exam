1a7203ff08b355debfd7b5cf74b533d50594bf52
TrachukYulia <71709949+TrachukYulia@users.noreply.github.com>
2021-01-24 00:57:46 +0200

    Add files via upload

---
+++ andrey6.cpp
@@ -0,0 +1,64 @@
+#include "andrey6.h"
+int input(int &n)
+{
+	do
+	{
+		cout << "enter size " << endl; cin >> n;
+	} while (n <= 0);
+	return n;
+}
+int* create(int n)
+{
+	srand(time(NULL));
+	int* B = new int[n];
+	for (int i = 0; i < n; i++)
+		B[i] = rand() % 20 + 2;
+	return B;
+}
+void output(int* B, int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << setw(5) << B[i];
+	cout << endl;
+}
+int** create1(int* B, int n)
+{
+	int** arr = new int* [n];
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = new int[n];
+	    for (int j = 0 ; j < n; j++)
+		{
+			arr[i][j] = pow(B[j], (i+1));
+		}
+	}
+	return arr;
+}
+void output2(int** arr, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		
+		for (int j = 0; j < n; j++)
+		{
+			cout << setw(10) << arr[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+}
+void create2(int** arr, int n)
+{
+	
+	for (int i = 0; i < n; i++)
+	{
+	
+		for (int j = 0; j < i; j++)
+		{
+			int tmp = arr[j][i];
+			arr[j][i] = arr[i][j];
+			arr[i][j] = tmp;
+		}
+	}
+}
---
+++ ctepinb.cpp
@@ -0,0 +1,12 @@
+﻿#include "andrey6.h"
+int main()
+{
+	int n = input(n);
+	int *B = create(n);
+	output(B, n);
+	int** arr = create1(B, n);
+	output2(arr, n);
+	create2(arr, n);
+	output2(arr, n);
+	return 0;
+}
